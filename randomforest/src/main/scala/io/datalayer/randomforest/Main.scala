abbd103788ec80691fc6d26be1b3d343cb1ebf86
Manuel Tondeur <manuel@datalayer.io>
2014-12-15 16:33:37 +0100

    RF-MODEL: Add accuracy metric

--- randomforest/src/main/scala/io/datalayer/randomforest/Main.scala
+++ randomforest/src/main/scala/io/datalayer/randomforest/Main.scala
@@ -87,7 +87,10 @@ object dataGenerator {
 // Helper to compute various metrics...
 // will be extend later...
 object Metrics {
-  def accuracy() = {}
+  def accuracy(pred: Seq[Label], labels: Seq[Label]): Double = {
+    pred.zip(labels).filter(x => x._1 == x._2).size.toDouble / pred.length
+  }
+
   def auc() = {}
   def entropy() = {}
   def gini() = {}
