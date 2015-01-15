efb1a082f798a1661484a8234fe729095155b81d
Vincent Botta <vince.bot@gmail.com>
2015-01-15 11:57:36 +0100

    Hadoop-summit: standalone with and without split almost done

--- src/test/scala/io/datalayer/randomforest/HadoopSummitTest.scala
+++ src/test/scala/io/datalayer/randomforest/HadoopSummitTest.scala
@@ -29,31 +29,75 @@ object TestParams {
 
 class StandaloneTest extends FunSuite {
   test("Standalone dla Random Forest") {
-    val trees = new Forest(min_samples_split=1000,n_estimators=10,max_features=2)
+    val trees = new Forest(min_samples_split=10,n_estimators=10,max_features=10)
     //val labeled = dataGenerator.genLabeled(100,50)
     //val labeled = new Data
     //labeled.loadCSV("/Users/botta/wrk/datalayer/events/20150415-hadoop-summit/src/data.csv",0)
     //labeled.describe
-    //trees.fit(labeled)    
+    //trees.fit(labeled)        
 
-    val x = Source.fromFile("/Users/botta/wrk/datalayer/events/20150415-hadoop-summit/src/data.csv").getLines() map {
+    /*
+    // READ FROM CSV
+    // TODO: there is something wrong here
+    val data = Source.fromFile("/Users/botta/wrk/datalayer/events/20150415-hadoop-summit/src/data.csv").getLines() map {
       line => val fields = line.split(" ")
         Labeled(
           fields.drop(1).map(_.toFloat).toList.toSeq,
           Label(fields(0).toInt)
           )
     }
+    */
 
-    trees.fit(x.toSeq.slice(0,4000))
-    val accuracy = trees.predictEval(x.toSeq.slice(6000,7000))
+    val data = dataGenerator.genLabeled(2000,50) 
+
+    val train = data.toSeq.slice(1000,2000)    
+    trees.fit(train)
+
+    val test1 = data.toSeq.slice(0,1000)    
+    var accuracy = trees.predictEval(test1)
     info("Accuracy = " + accuracy._2)
-    //trees.display
-    //accuracy._1.foreach(println)
     
+    val test2 = data.toSeq.slice(1000,2000)    
+    var accuracy2 = trees.predictEval(test2)
+    info("Accuracy = " + accuracy2._2)
+
+    // TODO: what is going on with the accuracy?    
     
+  }
+}
+
+  class StandaloneSplitTest extends FunSuite {
+    test("Standalone dla Random Forest with Split") {    
+
+    /*
+    // READ FROM CSV
+    // TODO: there is something wrong here
+    val data = Source.fromFile("/Users/botta/wrk/datalayer/events/20150415-hadoop-summit/src/data.csv").getLines() map {
+      line => val fields = line.split(" ")
+        Labeled(
+          fields.drop(1).map(_.toFloat).toList.toSeq,
+          Label(fields(0).toInt)
+          )
+    }
+    */
+
+    val data = dataGenerator.genLabeled(2000,50)    
+
+    val trees1 = new Forest(min_samples_split=10,n_estimators=10,max_features=10) 
+    val data1 = data.toSeq.slice(0,1000)//.toList.toSeq    
+    trees1.fit(data1)    
+    info("acc: " + trees1.predictEval(data.toSeq.slice(1000,2000))._2)
     
-    //info("test")
-    //assert(1 == 1)
+    val trees2 = new Forest(min_samples_split=10,n_estimators=10,max_features=10) 
+    val data2 = data.toSeq.slice(1000,2000)
+    trees2.fit(data2)
+    info("acc: " + trees2.predictEval(data.toSeq.slice(0,1000))._2)
+
+    // WARNING: forest parameters are not updated here
+    val ensemble = new Forest() 
+    ensemble.trees = Array.concat(trees1.trees, trees2.trees)
+    info("acc: " + ensemble.predictEval(data.toSeq.slice(0,2000))._2)
+
   }
 }
 
