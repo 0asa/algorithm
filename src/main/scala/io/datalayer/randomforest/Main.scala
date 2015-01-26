eb7ce11c4768de5e355512a8fe45ec2a0c2bb572
0asa <vince.bot@gmail.com>
2015-01-26 12:51:51 +0100

    RF: main updated

--- src/main/scala/io/datalayer/randomforest/Main.scala
+++ src/main/scala/io/datalayer/randomforest/Main.scala
@@ -13,4 +13,11 @@ object Main extends App {
     // Training the model
     forest.fit(train)
     println("Accuracy = " + forest.predictEval(test)._2)
+
+    val trees = new Forest(min_samples_split=10,n_estimators=10,max_features=5)
+    val labeled = dataGenerator.genData(50,10,true)
+    trees.fit(labeled)
+
+    val preds = trees.predict(labeled)
+    println("Score: " + trees.score(preds, labeled.getLabels()))
 }
