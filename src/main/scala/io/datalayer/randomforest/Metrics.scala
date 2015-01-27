36d2600a5c33a0dcbc333e69d945458cb968844a
Vincent Botta <vince.bot@gmail.com>
2015-01-27 10:10:57 +0100

    RF: more comments

--- src/main/scala/io/datalayer/randomforest/Metrics.scala
+++ src/main/scala/io/datalayer/randomforest/Metrics.scala
@@ -1,13 +1,16 @@
 package io.datalayer.randomforest
 
-// Helper to compute various metrics...
-// will be extend later...
+/** Helper to compute various metrics 
+  *	(such as accuracy, auc, log loss, confusion matrix, ...)
+  *
+  * TODO: Should be extended later.
+  */
 object Metrics {
   def accuracy(pred: Seq[Label], labels: Seq[Label]): Double = {
     pred.zip(labels).filter(x => x._1 == x._2).size.toDouble / pred.length
   }
 
-  def auc() = {}
+  def auc(): Double = { 0.0 }
   def entropy() = {}
   def gini() = {}
   def variance() = {}
